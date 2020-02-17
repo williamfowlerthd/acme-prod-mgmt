8a317194476801fec787c39652d8a59392bc3959
William Fowler <william_m_fowler@homedepot.com>
2020-02-17 14:22:01 -0500

    commit this ish

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -2,35 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  pageTitle: string = 'Product Detail';
-  product: IProduct;
+  pageTitle = 'Product Detail';
+  errorMessage = '';
+  product: IProduct | undefined;
 
   constructor(private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,
+              private productService: ProductService) {
+  }
 
   ngOnInit() {
-    let id = +this.route.snapshot.paramMap.get('id');
-    this.pageTitle += `: ${id}`;
-    this.product = {
-      'productId': id,
-      'productName': 'Leaf Rake',
-      'productCode': 'GDN-011',
-      'releaseDate': 'March 19, 2019',
-      'description': 'Leaf rake with 48-inch wooden handle.',
-      'price': 19.95,
-      'starRating': 3.2,
-      'imageUrl': 'assets/images/leaf_rake.png'
+    const param = this.route.snapshot.paramMap.get('id');
+    if (param) {
+      const id = +param;
+      this.getProduct(id);
     }
   }
 
-  onBack(): void {
-    this.router.navigate(['/products'])
+  getProduct(id: number) {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,47 +1,43 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { IProduct } from './product';
-import { ProductService } from './product-service';
-
-
+import { ProductService } from './product.service';
 
 @Component({
-  // selector: 'pm-products',
   templateUrl: './product-list.component.html',
-  providers: [ProductService],
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit{
-  pageTitle: string = 'Product List';
-  imageWidth: number = 50;
-  imageMargin: number = 2;
-  showImage: boolean = true;
-  errorMessage: string;
+export class ProductListComponent implements OnInit {
+  pageTitle = 'Product List';
+  imageWidth = 50;
+  imageMargin = 2;
+  showImage = false;
+  errorMessage = '';
 
-_listFilter: string;
-get listFilter(): string{
-  return this._listFilter;
-}
-set listFilter(value:string){
-  this._listFilter = value;
-  this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
-}
+  _listFilter = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
 
-  filteredProducts: IProduct[];
+  filteredProducts: IProduct[] = [];
   products: IProduct[] = [];
 
   constructor(private productService: ProductService) {
-    
-    // this.listFilter = 'cart';
+
   }
 
   onRatingClicked(message: string): void {
     this.pageTitle = 'Product List: ' + message;
   }
 
-  performFilter(filterBy:string): IProduct[]{
+  performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.products.filter((product: IProduct) =>
-    product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
   }
 
   toggleImage(): void {
@@ -51,11 +47,10 @@ set listFilter(value:string){
   ngOnInit(): void {
     this.productService.getProducts().subscribe({
       next: products => {
-        this.products = products
-        this.filteredProducts = this.products
+        this.products = products;
+        this.filteredProducts = this.products;
       },
       error: err => this.errorMessage = err
-    })
-   
+    });
   }
 }
